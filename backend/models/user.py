3ef740fe4015fe132d36a7376a5a7da3f1bae716
gustfernandez <gustavo.fernandez@uc.cl>
2024-01-10 01:39:56 -0300

    feat: add profile model class

--- backend/models/user.py
+++ backend/models/user.py
@@ -2,15 +2,26 @@
 Archivo de EJEMPLO para definir el modelo de usuario
 """
 
-from database import Base
-from sqlalchemy import Column, Integer, String
+from db.database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String)
     hashed_password = Column(String)
+    profile = relationship("Profile", back_populates="user", uselist=False)
+
+class Profile(Base):
+    __tablename__ = "profiles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    lastname = Column(String, nullable=False)
+    role = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user = relationship("User", back_populates="profile")
