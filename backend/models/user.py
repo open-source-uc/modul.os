e4cacb72d916f57c3b902640231bb5f3637f3f91
gustfernandez <gustavo.fernandez@uc.cl>
2024-01-10 01:47:44 -0300

    feat: define model in profile

--- backend/models/user.py
+++ backend/models/user.py
@@ -3,9 +3,13 @@ Archivo de EJEMPLO para definir el modelo de usuario
 """
 
 from db.database import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum
 from sqlalchemy.orm import relationship
+import enum
 
+class Role(enum.Enum):
+    user = 1
+    admin = 2
 
 class User(Base):
     __tablename__ = "users"
@@ -14,6 +18,7 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String)
     hashed_password = Column(String)
+    # en Relationship uselist=False para que sea 1 a 1. Pero, User puede tener varios perfiles?
     profile = relationship("Profile", back_populates="user", uselist=False)
 
 class Profile(Base):
@@ -22,6 +27,6 @@ class Profile(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     lastname = Column(String, nullable=False)
-    role = Column(String, nullable=False)
+    role = Column(Enum(Role))
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     user = relationship("User", back_populates="profile")
